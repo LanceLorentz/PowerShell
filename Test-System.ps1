e55a7d4b15b36d5edccd75a9bb06b89fa56c0830
LanceLorentz <112660015+LanceLorentz@users.noreply.github.com>
2022-09-01 13:55:04 -0600

    Add files via upload

---
+++ Test-System.ps1
@@ -0,0 +1,25 @@
+﻿Function Test-System
+{
+[CmdletBinding()]
+
+$IpAddress = Get-NetIPAddress | Where -Property 'InterfaceAlias' -EQ 'Wi-Fi'
+$Ping = Test-NetConnection Google.com
+$Properties = @{
+                'IPv4_Address' = $IpAddress.IPv4Address;
+                'IPv6_Address' = $IpAddress.IPv6Address;
+                'Internet_Connection' = $Ping.PingSucceeded
+               }
+$Output = New-Object -TypeName PSObject -Property $Properties
+Write-Output $Output
+<#
+.SYNOPSIS
+This command returns basic information regarding the local systems network connectivity.
+.DESCRIPTION 
+This command retunrs the following information:
+    - IPv4 Address
+    - IPv6 Address
+    - Internet Connectivity Status (True/False)
+            - Performs a Test-NetConnection to Google.com to verify internet connectivity
+This command functions only for the local host.
+#>     
+}
