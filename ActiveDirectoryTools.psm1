e8f2b4a888fe1f1a20068cbbde0dc3e03b6588e4
LanceLorentz <lance.c.lorentz@outlook.com>
2022-09-13 16:22:42 -0600

    Refinement

--- ActiveDirectoryTools.psm1
+++ ActiveDirectoryTools.psm1
@@ -5,14 +5,16 @@
 
 # Creates a new AD User in the specified Organizational Unit, and adds that user to the associated group.
 # Creates a temporary account password of "Pa55w.rd" that must be changed upon initial sign in.
-# This function was created in a test environment. To use in a production environment, edit the -Path value to meet your needs.
+# This function was created in a test environment. To use in a production environment, edit the -Path value and user principal name.
+# Edit SamAccountName to suit any naming conventions you may have, this was written assuming an already established naming convention.
 Function New-User
 {
 Param(
       [Parameter(Mandatory=$True)][String]$FirstName,
       [Parameter(Mandatory=$True)][String]$LastName, 
-      [Parameter(Mandatory=$False)][String]$Unit
+      [Parameter(Mandatory=$False)][String]$Department
       )
+      # This assumes a previously established naming convention for the SamAccountName, edit according to your company
       $SamAccountName = "$FirstName $LastName"
                                       
     If (Get-ADUser -filter {SamAccountName -eq $SamAccountName})
@@ -22,7 +24,7 @@ Param(
     
     Else
     {                                             
-    New-ADUser -Path "OU=$Unit,DC=Adatum,DC=com" `
+    New-ADUser -Path "OU=$Department,DC=Adatum,DC=com" `
     -Name "$FirstName $LastName" `
     -GivenName $FirstName `
     -Surname $LastName `
@@ -31,9 +33,14 @@ Param(
     -UserPrincipalName "$FirstName$LastName@adatum.com" `
     -AccountPassword (ConvertTo-SecureString "Pa55w.rd" -AsPlainText -Force) `
     -ChangePasswordAtLogon $True `
+    -Department $Department `
+    -Company "Adatum" `
     -Enabled $True
   
-    Add-ADGroupMember -Members "$FirstName $LastName" -Identity $Unit
+    # Member name must match SamAccountName, edit this line if your SamAccountName follows different naming conventions
+    Add-ADGroupMember -Members "$FirstName $LastName" -Identity $Department
+
+    Write-Host "The user $FirstName $LastName has been created"
     }
 
  <#
@@ -67,21 +74,32 @@ Param(
 # The Security Group is created with the scope: Global and Category: Security. The group is placed within the created OU.
 # PRIOR TO USE IN A PRODUCTION ENVIRONMENT: Change -Path to the appropriate path for your Active Directory environment.
 # If desired, you can change the group scope and category to better suit your needs.
-Function New-Unit
+Function New-Department
 {
 [CmdletBinding()]
     Param(
-            [Parameter(Mandatory=$True)][String]$Name
+          [Parameter(Mandatory=$True)][String]$Name
           )
+New-ADOrganizationalUnit `
+-Name $Name `
+# Edit path to suit your production environment
+-Path "DC=Adatum,DC=com"
 
-New-ADOrganizationalUnit -Name $Name -Path "DC=Adatum,DC=com"
-New-ADGroup -Name $Name  -GroupScope Global -GroupCategory Security -Path "OU=$Name,DC=Adatum,DC=com"
-<#
+New-ADGroup `
+-Name $Name  `
+-GroupScope Global `
+-GroupCategory Security `
+# Edit Path to suit your prduction environment
+-Path "OU=$Name,DC=Adatum,DC=com"
+
+Write-Host "The organizational unit $Name has been created."
+Write-Host "The security group $Name has been created."
+ <#
 .SYNOPSIS
 Creates a new organizational unit along with a global security group within the created OU.
 
 .DESCRIPTION
-Creates a new Organizational Unit as well as a global security group with the same name. Places the security group within the created OU.
+ Creates a new Organizational Unit as well as a global security group with the same name. Places the security group within the created OU.
 Specifiy the desried name of the OU and security group with the -Name parameter.
 
 .PARAMETER Name
@@ -94,5 +112,5 @@ New-Unit -Name Sales
 Creates an Organizational Unit with the name Sale, and creates a global security group with the name Sales within that OU.
 
 .NOTES
-#>
-}
+ #>
+ }
