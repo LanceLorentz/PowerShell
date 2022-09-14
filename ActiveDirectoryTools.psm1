9a0ef2ca35eed143e2babcdcdb43098b99db0fcf
LanceLorentz <lance.c.lorentz@outlook.com>
2022-09-14 16:28:28 -0600

    Added Import-Users to add bulk AD users from a CSV

--- ActiveDirectoryTools.psm1
+++ ActiveDirectoryTools.psm1
@@ -70,6 +70,77 @@ Param(
 
 
 
+# The following function imports users from a CSV and creates Active Directory accounts for each user. Each created user is placed in the appropriate OU and Security Group. 
+# This function assumes that the OU, Security Group, and Department for each user all use the same name. 
+# To edit this function for use in a production environment, edit the name/path of the CSV to import from, and adjust the variables/members as necessary to accomodate your CSV categories. 
+# Also ensure that SamAccountName follows your preferred naming conventions.
+# A temporary password of "Pa55w.rd" is created for each user that must be changed upon initial sign in.
+Function Import-Users 
+{
+    # Edit this line to match the filepath of your CSV
+    $ADUsers = Import-CSV C:\Banshees.csv
+
+    Foreach ($user in $ADUsers)
+    {
+        # Edit to match the criteria used in your CSV
+        $FirstName = $User.First
+        $LastName = $User.Last
+        $Department = $User.Department
+        $Job = $User.Job
+        $Company = $User.Company
+        $State = $User.State
+        $Phone = $User.Phone
+        $SamAccountName = ($User.First)+($User.Last)
+    
+        # Assumes an already established naming convention for SamAccountName, edit to match your naming convention
+        If (Get-ADUser -Filter {SamAccountName -eq $SamAccountName}) 
+        {
+        Write-Warning "User account for $FirstName $LastName already exists in ACtive Directory."
+        }
+
+        Else
+        {
+        # This assumes that the user's department, OU, and security group all use the same name
+        # Edit email address to fit the needs of your environment if necessary
+        New-ADUser `
+        -Name "$FirstName $LastName" `
+        -GivenName $FirstName `
+        -Surname $LastName `
+        -SamAccountName "$FirstName$LastName" `
+        -UserPrincipalName "$FirstName$LastName@$Company.com" `
+        -DisplayName "$FirstName $LastName" `
+        -Title $Job `
+        -State $State `
+        -Company $Company `
+        -OfficePhone $Phone `
+        -EmailAddress "$FirstName.$LastName@$Company.com" `
+        -AccountPassword (ConvertTo-SecureString "Pa55w.rd" -AsPlainText -Force) `
+        -ChangePasswordAtLogon $True `
+        -Enabled $True `
+        -Path "OU=$Department,DC=$Company,DC=Com"
+
+        Add-ADGroupMember -Members "$FirstName$LastName" -Identity $Department
+
+        Write-Host "The user $FirstName $LastName has been created."
+        }   
+    }
+
+<#
+.SYNOPSIS
+Imports new Active Directory Users from a CSV.
+.DESCRIPTION 
+This function imports users from a CSV and creates user accounts in ACtive Directory. The created users are also placed in the appropriate organizational unit and corresponding security group.
+Creates a temporary password of "Pa55w.rd" for each created account that must be changed at initial account sign in.
+The function also checks to see if there is already an existing account with the desired name. If an account already exists than a warning message is returned.
+If the account is created properly than an output indicating that the account has been created is returned.
+.NOTES
+This function assumes that the user's OU, security group, and department all use the same name. If they do not, then this will need to be changed.
+Variables and other criteria may need to be edited in order to fit your environment and CSV format.
+#>
+}
+
+
+
 # The following function is used to create a new Organizational Unit and Secrity Group in Active Driectory.
 # The Security Group is created with the scope: Global and Category: Security. The group is placed within the created OU.
 # PRIOR TO USE IN A PRODUCTION ENVIRONMENT: Change -Path to the appropriate path for your Active Directory environment.
